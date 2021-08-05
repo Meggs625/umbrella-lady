d0826adda117dfe18a16a83d3e5a29a095af44d0
Megan McBride <megan.mcbride@colorado.edu>
2021-08-05 15:25:31 -0600

    Adjust HTML file to allow for styling and begin to check functionality in scripts

--- src/scripts.js
+++ src/scripts.js
@@ -7,6 +7,14 @@ import './css/base.scss';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 // import './images/turing-logo.png'
 import './images/icons8-umbrella-48.png';
+import './images/icons8-umbrella-48 (1).png';
 import './images/icons8-user-30.png';
 
-console.log('This is the JavaScript entry file - your code begins here.');
+const myTripsBtn = document.getElementById('my-trips-btn')
+
+
+myTripsBtn.addEventListener('click', renderTripsPage)
+
+function renderTripsPage() {
+  
+}
