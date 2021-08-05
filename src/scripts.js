05e963cadceee3dcd798a2ebed9e7df50b1b4f2f
Megan McBride <megan.mcbride@colorado.edu>
2021-08-05 16:01:25 -0600

    Include functionality to toggle to the My Trips page which will be styled next

--- src/scripts.js
+++ src/scripts.js
@@ -3,6 +3,7 @@
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
+import domUpdates from './domUpdates';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 // import './images/turing-logo.png'
@@ -10,11 +11,13 @@ import './images/icons8-umbrella-48.png';
 import './images/icons8-umbrella-48 (1).png';
 import './images/icons8-user-30.png';
 
-const myTripsBtn = document.getElementById('my-trips-btn')
+const myTripsBtn = document.getElementById('my-trips-btn');
+const dashboard = document.getElementById('the-dashboard');
+const myTrips = document.getElementById('trips-page')
 
 
 myTripsBtn.addEventListener('click', renderTripsPage)
 
 function renderTripsPage() {
-  
+  domUpdates.toggleView(myTrips, dashboard)
 }
