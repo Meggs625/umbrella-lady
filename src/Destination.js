fa45d6b34c5aab611432af399f377249cce79485
Megan McBride <megan.mcbride@colorado.edu>
2021-08-04 19:32:49 -0600

    Build the properties and methods of the destination class as well as the cooresponding test

---
+++ src/Destination.js
@@ -0,0 +1,17 @@
+class Destination {
+  constructor(theDestination) {
+    this.id = theDestination.id;
+    this.name = theDestination.destination;
+    this.lodgingCost = theDestination.estimatedLodgingCostPerDay;
+    this.flightCost = theDestination.estimatedFlightCostPerPerson;
+    this.image = theDestination.image;
+    this.altText = theDestination.alt;
+  }
+
+  returnCosts() {
+    return [this.lodgingCost, this.flightCost];
+  }
+
+}
+
+export default Destination; 
