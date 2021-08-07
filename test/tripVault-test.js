ce5ef3e2105f931db26677c993e95945972114c8
Megan McBride <megan.mcbride@colorado.edu>
2021-08-07 17:45:55 -0600

    Add new tests for new methods and ensure all test are still passing

--- test/tripVault-test.js
+++ test/tripVault-test.js
@@ -11,15 +11,50 @@ describe('Trip Vault', () => {
   })
 
   it('should be a function', () => {
-    expect(TripVault).to.be.a('function')
+    expect(TripVault).to.be.a('function');
   })
 
   it('should be an instance of Trip Vault', () => {
-    expect(newVault).to.be.an.instanceOf(TripVault)
+    expect(newVault).to.be.an.instanceOf(TripVault);
   })
 
   it('should house all trip data', () => {
-    expect(newVault.trips).to.deep.equal(sampleTripData)
+    expect(newVault.trips).to.deep.equal(sampleTripData);
+  })
+
+  it('should return all of the trips for one specified user', () => {
+    const travelerTrips = newVault.findTravelerTripsById(2);
+    expect(travelerTrips).to.deep.equal([
+      {
+        "id": 2,
+        "userID": 2,
+        "destinationID": 25,
+        "travelers": 5,
+        "date": "2022/10/04",
+        "duration": 18,
+        "status": "approved",
+        "suggestedActivities": []
+      },
+      {
+        "id": 6,
+        "userID": 2,
+        "destinationID": 35,
+        "travelers": 3,
+        "date": "2022/06/29",
+        "duration": 9,
+        "status": "pending",
+        "suggestedActivities": []
+      },{
+        "id": 10,
+        "userID": 2,
+        "destinationID": 50,
+        "travelers": 6,
+        "date": "2022/07/23",
+        "duration": 17,
+        "status": "approved",
+        "suggestedActivities": []
+      }
+    ]);
   })
 
   it('should calculate the total cost for a trip based on trip id', () => {
@@ -32,10 +67,10 @@ describe('Trip Vault', () => {
       "image": "https://images.unsplash.com/photo-1555333145-4acf190da336?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
       "alt": "lit up city at night"
     }];
-    const newDestination = new DestinationCatalog(jakarta)
-    const locationCosts = newDestination.returnDestinationCosts(6)
-    const tripCost = newVault.calculateCostByDestination(locationCosts)
-    expect(tripCost).to.equal(3630)
+    const newDestination = new DestinationCatalog(jakarta);
+    const locationCosts = newDestination.returnDestinationCosts(6);
+    const tripCost = newVault.calculateCostByDestination(locationCosts);
+    expect(tripCost).to.equal(3630);
   })
 
 })
--- test/trips-test.js
+++ test/trips-test.js
@@ -54,36 +54,41 @@ describe('Trips', () => {
   })
 
   it('should be an instance of the Trip class', () => {
-    expect(myTrips).to.be.an.instanceOf(Trips)
+    expect(myTrips).to.be.an.instanceOf(Trips);
   })
 
   it('should store one travelers trip data', () => {
-    expect(myTrips.trips).to.deep.equal(tripInfo)
+    expect(myTrips.trips).to.deep.equal(tripInfo);
   })
 
   it('should return all the trips with a certain status', () => {
     const pendingTrips = myTrips.findTripsByStatus('pending');
-    expect(pendingTrips).to.deep.equal([tripInfo[0], tripInfo[3]])
+    expect(pendingTrips).to.deep.equal([tripInfo[0], tripInfo[3]]);
   })
 
   it('should return all the trips with a different status', () => {
     const pendingTrips = myTrips.findTripsByStatus('approved');
-    expect(pendingTrips).to.deep.equal([tripInfo[1], tripInfo[2]])
+    expect(pendingTrips).to.deep.equal([tripInfo[1], tripInfo[2]]);
   })
 
   it('should return trips already taken', () => {
     const pastTrips = myTrips.findTripsByDate('2021/08/03', 'past');
-    expect(pastTrips).to.deep.equal([tripInfo[1]])
+    expect(pastTrips).to.deep.equal([tripInfo[1]]);
   })
 
   it('should return future trips', () => {
     const futureTrips = myTrips.findTripsByDate('2021/08/03', 'future');
-    expect(futureTrips).to.deep.equal([tripInfo[0], tripInfo[2]])
+    expect(futureTrips).to.deep.equal([tripInfo[2]]);
   })
 
   it('should return a trip of the same date', () => {
-    const thisTrip = myTrips.findTripsByDate('2021/08/03', 'current')
-    expect(thisTrip).to.deep.equal(tripInfo[3])
+    const thisTrip = myTrips.findTripsByDate('2021/04/30', 'current');
+    expect(thisTrip).to.deep.equal([tripInfo[1]]);
+  })
+
+  it('should not include pending trips in the date search', () => {
+    const futureTrips = myTrips.findTripsByDate('2017/01/01', 'future');
+    expect(futureTrips).to.deep.equal([tripInfo[1], tripInfo[2]]);
   })
 
   it('should calculate total cost for traveling within a given year', () => {
@@ -127,7 +132,59 @@ describe('Trips', () => {
     ])
     const thisYearsTravels = 
       myTrips.calculateAnnualTripCosts('2021', destinationRepo);
-    expect(thisYearsTravels).to.equal(13330)
+    expect(thisYearsTravels).to.equal(14663)
   })
 
+  it('should calculate the cost for a newly selected trip', () => {
+    const newTrip = {
+      id: 14,
+      userID: 12,
+      destinationID: 19,
+      travelers: 5,
+      date: '2019/03/08',
+      duration: 4,
+      status: 'pending',
+      suggestedActivities: ['walking', 'relaxing']
+    };
+    let destinationRepo = new DestinationCatalog([
+      {
+        "id": 49,
+        "destination": "Castries, St Lucia",
+        "estimatedLodgingCostPerDay": 650,
+        "estimatedFlightCostPerPerson": 90,
+        // eslint-disable-next-line max-len
+        "image": "https://images.unsplash.com/photo-1524478075552-c2763ea171b8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1502&q=80",
+        "alt": "aerial photography of rocky mountain under cloudy sky"
+      },
+      {
+        "id": 29,
+        "destination": "Willemstad, Curaçao",
+        "estimatedLodgingCostPerDay": 80,
+        "estimatedFlightCostPerPerson": 1100,
+        // eslint-disable-next-line max-len
+        "image": "https://images.unsplash.com/photo-1541748603027-cbfefa3a6c8f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1952&q=80",
+        "alt": "brightly colored buildings near body of water"
+      },
+      {
+        "id": 19,
+        "destination": "Quito, Ecuador",
+        "estimatedLodgingCostPerDay": 60,
+        "estimatedFlightCostPerPerson": 500,
+        // eslint-disable-next-line max-len
+        "image": "https://images.unsplash.com/photo-1501684691657-cf3012635478?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80",
+        "alt": "a city at night with cloudy, snowy mountains in the distance"
+      },
+      {
+        "id": 6,
+        "destination": "Jakarta, Indonesia",
+        "estimatedLodgingCostPerDay": 70,
+        "estimatedFlightCostPerPerson": 890,
+        // eslint-disable-next-line max-len
+        "image": "https://images.unsplash.com/photo-1555333145-4acf190da336?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+        "alt": "lit up city at night"
+      }
+    ]);
+    const newCost = myTrips.calculateNewTripCost(newTrip, destinationRepo);
+    expect(newCost).to.equal(3014);
+  })
 })
